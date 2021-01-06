489e566573a6ead0c8abdf55a488a9e9ed38bb06
gursi26 <gursimar.batra@gmail.com>
2021-01-06 14:23:21 +0800

    logistic regression

--- Pytorch/logistic_regression/LOG_REG.py
+++ Pytorch/logistic_regression/LOG_REG.py
@@ -1,2 +1,33 @@
 import torch
 from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader
+
+
+class Data(Dataset):
+
+    def __init__(self):
+        pass
+    
+    def __getitem__(self,index):
+        pass
+
+    def __len__(self):
+        pass
+
+
+class log_reg(nn.Module):
+
+    def __init__(self, input_size, output_size):
+        super(log_reg, self).__init__()
+        self.linear = nn.Linear(input_size, output_size)
+
+    def forward(self,x):
+        yhat = torch.sigmoid(self.linear(x)) # Sigmoid function for logistic regression
+        return yhat
+
+input_size = 
+output_size = 1
+model1 = log_reg(input_size, output_size)
+model2 = nn.Sequential(nn.Linear(input_size, output_size), nn.Sigmoid())
+
+    
